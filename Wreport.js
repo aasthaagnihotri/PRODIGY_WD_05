cf76dcbbfdc773899ccd4c5e1dbc6dc6eb6f4740
aasthaagnihotri <168187064+aasthaagnihotri@users.noreply.github.com>
2024-06-19 17:43:44 +0530

    Add files via upload

--- Wreport.js
+++ Wreport.js
@@ -15,12 +15,13 @@ let weather_wind = document.querySelector('.weather_wind');
 let weather_pressure = document.querySelector('.weather_pressure');
 
 // search
-document.querySelector(".weather_search").addEventListener('submit', e => {
+document.querySelector(".weather_searchform").addEventListener('submit', e => {
     let search = document.querySelector(".weather_search");
     // prevent default action
     e.preventDefault();
     // change current city
     currCity = search.value;
+     
     // get weather forecast 
     getWeather();
     // clear form
@@ -46,26 +47,10 @@ document.querySelector(".weather_unit_farenheit").addEventListener('click', () =
     }
 })
 
-function convertTimeStamp(timestamp, timezone){
-     const convertTimezone = timezone / 3600;
-
-      // convert seconds to hours 
-
-    const date = new Date(timestamp * 1000);
-    
-    const options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        timeZone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}${Math.abs(convertTimezone)}`,
-        hour12: true,
-    }
-    return date.toLocaleString("en-US", options)
-   
-}
+const convertTimestamp = (timestamp, timezoneOffset) => {
+    const date = new Date((timestamp + timezoneOffset) * 1000);
+    return date.toUTCString();
+};
 
  
 
@@ -76,21 +61,24 @@ function convertCountryCode(country){
 }
 
 function getWeather(){
-    const API_KEY = '579e062e769e36b95270ec73fa9fd25f'
+    const API_KEY = '579e062e769e36b95270ec73fa9fd25f';
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=${API_KEY}&units=metric`).then(res => res.json()).then
-    (data => console.log(data))
-    city.innerHTML = `${data.name},${convertCountryCode(data.sys.country)}`
-    datetime.innerHTML = convertTimeStamp(data.dt, data.timezone); 
-    weather_forecast.innerHTML = `<p>${data.weather[0].main}`
-    weather_temperature.innerHTML = `${data.main.temp.toFixed()}&#176`
-    weather_icon.innerHTML = `<img src=" https://openweathermap.org/img/wn/{data.weather{0}.icon}@4x.png"/>`
-    weather_minmax.innerHTML = `<p>Min: ${data.main.temp_min.toFixed()}&#176</p>
-    <p>Max: ${data.main.temp_max.toFixed()}&#176</p>`
-    weather_realfeel.innerHTML = `${data.main.feels_like.toFixed()}&#176`
-    weather_humidity.innerHTML = `${data.main.humidity}%`
-    weather_wind.innerHTML = `${data.wind.speed} ${units === "imperial" ? "mph": "m/s"}` 
-    weather_pressure.innerHTML = `${data.main.pressure} hPa`
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currCity}&appid=${API_KEY}&units=${units}`).then(res => res.json()).then
+    (data => {
+        console.log(data)
+        city.innerHTML = `${data.name},${convertCountryCode(data.sys.country)}`
+        datetime.innerHTML = convertTimestamp(data.dt, data.timezone); 
+        weather_forecast.innerHTML = `<p>${data.weather[0].main}`
+        weather_temperature.innerHTML = `${data.main.temp.toFixed()}&#176`
+        weather_icon.innerHTML = `   <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png" />`
+        weather_minmax.innerHTML = `<p>Min: ${data.main.temp_min.toFixed()}&#176</p>
+        <p>Max: ${data.main.temp_max.toFixed()}&#176</p>`
+        weather_realfeel.innerHTML = `${data.main.feels_like.toFixed()}&#176`
+        weather_humidity.innerHTML = `${data.main.humidity}%`
+        weather_wind.innerHTML = `${data.wind.speed} ${units === "imperial" ? "mph": "m/s"}` 
+        weather_pressure.innerHTML = `${data.main.pressure} hPa`
+    })
+    
 }
 
 
